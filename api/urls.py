a79a575e3846f7326200d14f6af168b4aaf2d223
Joshua Penhaligon <j.penhaligon@se22.qmul.ac.uk>
2024-12-19 18:16:11 +0000

    (read description) Added authentication, user profile apis, friend requests, serializing, some tests.
    i will need to change this when the frontend is built. let me know when frontend (login, register, logout, friend requests forms etc) has been made so i can get this to work

--- api/urls.py
+++ api/urls.py
@@ -2,9 +2,18 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 from django.http import HttpResponse
+from django.urls import path
 
 from .views import main_spa
+from .views import signup_view, login_view, logout_view
+from .views import UserProfileAPIView
+from .views import FriendRequestAPIView
 
 urlpatterns = [
     path('', main_spa),
+    path('signup/', signup_view, name='signup'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('api/profile/', UserProfileAPIView.as_view(), name='profile'),
+    path('api/friend-requests/', FriendRequestAPIView.as_view(), name='friend_requests'),
 ]
